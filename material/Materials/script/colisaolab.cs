e794699ff4f1087abc53bb8a479aedcf69f79fbb
Gabriel Victor Dias Morais <1302793@sga.pucminas.br>
2020-09-25 19:17:20 -0300

    commit inicial

---
+++ material/Materials/script/colisaolab.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class colisaolab : MonoBehaviour
+{
+    public string fase;
+    void OnCollisionEnter(Collision outro){
+        SceneManager.LoadScene(fase);
+    }
+}
---
+++ material/Materials/script/movimentar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentar : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Debug.Log(h);
+        transform.Translate(h * speed * Time.deltaTime, 0, v* speed * Time.deltaTime);
+    }
+}
